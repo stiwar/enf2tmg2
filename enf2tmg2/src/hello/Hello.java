064032f7822d42e6afd87d9cb2893f2c8ab2782e
stiwar <lsarteaga@unicauca.edu.co>
2013-12-03 12:49:39 -0500

    Primer commit del proyecto enf2tmg2.

---
+++ enf2tmg2/src/hello/Hello.java
@@ -0,0 +1,17 @@
+package hello;
+
+public class Hello {
+
+	public Hello() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");	
+	}
+
+}
